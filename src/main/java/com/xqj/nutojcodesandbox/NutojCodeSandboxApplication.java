354e8578e8eff9baf68f2541351716a35b2648f5
xuqingjian <xuqingjian894@gmail.com>
2023-08-20 19:03:15 +0800

    init commit.

---
+++ src/main/java/com/xqj/nutojcodesandbox/NutojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.xqj.nutojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xqj/nutojcodesandbox/NutojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqj.nutojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
